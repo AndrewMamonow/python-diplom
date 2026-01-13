e9bb70e4e6d959b49650be18be77a037ae6d8434
Andrew_m62 <andrew.mamonow@yandex.ru>
2026-01-13 13:44:56 +0300

    add views

--- orders/backend/views.py
+++ orders/backend/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+
+from backend.models import (
+    User, 
+    Shop, 
+    Category, 
+    Product, 
+    ProductInfo, 
+    Parameter, 
+    ProductParameter, 
+    Order, 
+    OrderItem,
+    Contact, 
+    )
+
+from backend.serializers import (
+    UserSerializer, 
+    CategorySerializer, 
+    ShopSerializer, 
+    ProductInfoSerializer,
+    OrderItemSerializer, 
+    OrderSerializer, 
+    ContactSerializer,
+    )
+
+
+class CategoryView(ListAPIView):
+    """
+    Класс для просмотра категорий
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+class ShopView(ListAPIView):
+    """
+    Класс для просмотра списка магазинов
+    """
+    queryset = Shop.objects.filter(state=True)
+    serializer_class = ShopSerializer
