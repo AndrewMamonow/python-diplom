3be18beb161e66a34830ecf760956b6bf2ee5b31
Andrew_m62 <andrew.mamonow@yandex.ru>
2026-02-12 13:11:46 +0300

    Исправлен файл tasks

--- orders/backend/tasks.py
+++ orders/backend/tasks.py
@@ -1,9 +1,10 @@
 from celery import shared_task
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
-from django.utils import timezone
-from .models import Order
 from django.conf import settings
+
+from .models import Order
+
 
 @shared_task
 def send_order_confirmation_email(order_id):
